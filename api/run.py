7f68f37ded527ca3d5b5cfb5c1a4c582c1bd5111
Dmitriy Lefterov <62502633+DLineone@users.noreply.github.com>
2026-01-07 01:46:21 +0300

    feat(db): setup db

--- api/run.py
+++ api/run.py
@@ -3,10 +3,15 @@ import json
 from dotenv import load_dotenv
 from flask import Flask
 from app.routes.health import health_check_bp
+from app.db import db, migrate
 
 load_dotenv()
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db.init_app(app)
+migrate.init_app(app, db)
 
 app.register_blueprint(health_check_bp)
 
