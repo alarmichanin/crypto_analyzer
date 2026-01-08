bc20053a1818a08eee58cf27da113277832af0c0
Dmitriy Lefterov <62502633+DLineone@users.noreply.github.com>
2026-01-08 21:48:38 +0300

    fix tests

--- api/run.py
+++ api/run.py
@@ -8,7 +8,9 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL", "postgresql+psycopg2://postgres:postgres@db:5432/myappdb"
+)
 db.init_app(app)
 migrate.init_app(app, db)
 
